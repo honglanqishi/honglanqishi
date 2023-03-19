434b5502e2f628569e19c73996b60a9039094039
jiangsu <1071078@qq.com>
2023-03-19 12:08:38 +0800

    1

--- routes/users.js
+++ routes/users.js
@@ -4,13 +4,13 @@ let router = express.Router();
 
 router.post('/userInfo', async (req, res) => {
 
-   
+    console.log('openid', req.body.oepnid)
     let ret = await User.findAll({
         where: {
             openid: req.body.oepnid
-          }
+        }
     })
-    console.log(ret)
+    console.log(ret,'查询结果')
     // res.send(ret)
 });
 
