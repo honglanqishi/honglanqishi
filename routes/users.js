432e271edafdea9f37e0b5a09ac751b3bf64cc42
jiangsu <1071078@qq.com>
2023-03-19 12:45:31 +0800

    update

--- routes/users.js
+++ routes/users.js
@@ -2,17 +2,17 @@ const { User } = require("../db");
 var express = require('express');
 let router = express.Router();
 
-router.post('/userInfo', async (req, res) => {
+router.post('/getUserInfo', async (req, res) => {
 
     console.log('openid', req.body.openid)
-    let ret = await User.findAll({
+    let ret = await User.findOne({
         where: {
             openid: req.body.openid
         },
         raw:true
     })
     console.log(ret,'查询结果')
-    // res.send(ret)
+    res.send(ret)
 });
 
 
