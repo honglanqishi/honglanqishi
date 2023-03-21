1240a98c619284c6011c302d24d6ac487d53956c
jiangsu <1071078@qq.com>
2023-03-22 00:11:10 +0800

    13

--- chat/proxy.js
+++ chat/proxy.js
@@ -34,7 +34,7 @@ router.post('/getAnswer', async (req, res) => {
                 console.log('执行超时兼容操作')
                 let tempdata = await Timeou.create({
                     openid: req.body.openid,
-                    sessionId: req.body.sessionId||data.sessionId,
+                    sessionId: req.body.sessionId,
                     data: response.data.data
                 })
                 // setTimeout(async ()=>{
