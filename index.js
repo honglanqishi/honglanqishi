1650e744254b28af50bdd60fa2b69f0208aed890
jiangsu <1071078@qq.com>
2023-04-03 15:19:20 +0800

    2

--- index.js
+++ index.js
@@ -15,11 +15,15 @@ import { init, Counter } from './db.js'
 
 
 
+
+
+
+
 const logger = morgan("tiny");
 
 const app = express();
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
+app.use(express.urlencoded({ limit: '50mb',extended: false }));
+app.use(express.json({ limit: '50mb' }));
 
 
 app.use('/users',usersRouter)
